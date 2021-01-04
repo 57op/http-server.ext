36490dae153419c7f458c7105ccc294838d1a7fd
57op <57op@users.noreply.github.com>
2021-01-04 23:31:03 +0100

    [response] fix headers reusage bug

--- response.py
+++ response.py
@@ -1,22 +1,26 @@
 import json
 
 class Response:
-	def __init__(self, data, content_type, encoding='utf-8', headers={}):
+	def __init__(self, data, content_type, encoding='utf-8', headers=None):
 		if isinstance(data, str):
 			data = data.encode(encoding)
 
-		headers['Content-Type'] = f'{content_type}; charset={encoding}'
-		headers['Content-Length'] = len(data)
-
 		self.data = data
-		self.headers = headers
+
+		self.headers = {}
+		self.headers['Content-Type'] = f'{content_type}; charset={encoding}'
+		self.headers['Content-Length'] = len(self.data)
+
+		if isinstance(headers, dict):
+			for k, v in headers.items():
+				self.headers[k] = v
 
 class JsonResponse(Response):
-	def __init__(self, data, encoding='utf-8', headers={}):
+	def __init__(self, data, encoding='utf-8', headers=None):
 		super().__init__(json.dumps(data), 'application/json', encoding=encoding, headers=headers)
 
 class FileResponse(Response):
-	def __init__(self, file, content_type, encoding='utf-8', headers={}):
+	def __init__(self, file, content_type, encoding='utf-8', headers=None):
 		with open(file, 'rb') as fh:
 			page = fh.read()
 
